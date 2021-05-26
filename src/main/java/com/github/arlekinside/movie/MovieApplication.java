206cd1014d94ab3c746edcc062d2166d4fe91ea6
arlekinside <bohdan2003.ua@gmail.com>
2021-05-26 04:28:55 +0300

    Security removed

--- src/main/java/com/github/arlekinside/movie/MovieApplication.java
+++ src/main/java/com/github/arlekinside/movie/MovieApplication.java
@@ -1,10 +1,7 @@
 package com.github.arlekinside.movie;
 
-import com.github.arlekinside.movie.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MovieApplication {
