8cb7c7951e430e9f04ef9eb2a6d84cab6d9f6c14
arlekinside <bohdan2003.ua@gmail.com>
2021-05-26 13:42:49 +0300

    Revert "User controller fix"
    This reverts commit 0feaae6fefde75d38abefe91ed9ca0d9a97ee3b9.

--- src/main/java/com/github/arlekinside/movie/controllers/UserController.java
+++ src/main/java/com/github/arlekinside/movie/controllers/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public void updateUser(@RequestBody User user, @PathVariable Long id) {
+    public void updateUser(@RequestBody User user, @PathVariable String id) {
         if (user.getId().equals(id))
             service.updateUser(user);
     }
